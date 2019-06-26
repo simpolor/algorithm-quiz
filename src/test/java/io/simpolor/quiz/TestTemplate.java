97ba9bf2f23b663e814a74eef9dbf78a91198237
parksy <parksy@smilegate.com>
2019-06-26 15:33:07 +0900

    .

--- src/test/java/io/simpolor/quiz/TestTemplate.java
+++ src/test/java/io/simpolor/quiz/TestTemplate.java
@@ -10,8 +10,9 @@ public class TestTemplate {
     @Test
     public void test(){
 
-        int input = 108;
+        String input = "1A";
 
+        System.out.println(Integer.parseInt(input, 16)) ;
         // 8진수를 구는 공식
         /**
          * int input = 108;
@@ -23,19 +24,10 @@ public class TestTemplate {
          * System.out.println(1 % 8); // 1
          */
 
-        // System.out.println(Integer.toOctalString(input));
+        /// System.out.println(Integer.toOctalString(input));
         // System.out.println(Integer.toBinaryString(input));
 
-        List<Integer> list = new ArrayList();
-        do {
-            System.out.println("input : "+input % 8);
-            list.add(input % 8);
-            input = input / 8;
-        }while (input != 0);
 
-        for(int i=list.size()-1; 0<=i; i--){
-            System.out.print(list.get(i));
-        }
 
     }
 }
