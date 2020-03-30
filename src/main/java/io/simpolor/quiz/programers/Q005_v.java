a90dc892e4a338434cef76460752558a3b2ced26
parksy <parksy@smilegate.com>
2020-03-30 09:30:50 +0900

    .

---
+++ src/main/java/io/simpolor/quiz/programers/Q005_v.java
@@ -0,0 +1,33 @@
+package io.simpolor.quiz.programers;
+
+public class Q005_v {
+
+
+    /***
+     * 문제 설명
+     *
+     * 배열을 입력하면 4,3,2,1 중 가장 작은 수를 뺀 나머지 배열을 출력
+     *      * 혹시라노 빈배열 혹은 배열에 하나의 수만 들어가 있을 경우 -10을 리턴
+     *
+     */
+    public static void main(String[] args){
+
+        int[] n = new int[]{4, 5, 6};
+
+        Solution solution = new Solution();
+
+
+    }
+
+
+    public static class Solution {
+
+        public int[] solution(int[] n){
+
+
+
+            return n;
+        }
+
+    }
+}
