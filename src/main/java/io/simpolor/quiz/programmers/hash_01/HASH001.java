6ff007e8a753a0243b707395ef5c6ccafa579d36
parksy <parksy@smilegate.com>
2020-06-02 09:16:57 +0900

    .

--- src/main/java/io/simpolor/quiz/programmers/hash_01/HASH001.java
+++ src/main/java/io/simpolor/quiz/programmers/hash_01/HASH001.java
@@ -8,9 +8,11 @@ import java.util.*;
  * ------------------------------
  * 문제 설명
  * ------------------------------
- * 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+ * 수많은 마라톤 선수들이 마라톤에 참여하였습니다.
+ * 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
  *
- * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+ * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+ * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
  *
  * ------------------------------
  * 제한사항
@@ -23,10 +25,10 @@ import java.util.*;
  * ------------------------------
  * 입출력 예
  * ------------------------------
- * participant	completion	return
- * [leo, kiki, eden]	[eden, kiki]	leo
- * [marina, josipa, nikola, vinko, filipa]	[josipa, filipa, marina, nikola]	vinko
- * [mislav, stanko, mislav, ana]	[stanko, ana, mislav]	mislav
+ * participant,	    completion, 	return
+ * [leo, kiki, eden],	[eden, kiki],	leo
+ * [marina, josipa, nikola, vinko, filipa],	[josipa, filipa, marina, nikola],	vinko
+ * [mislav, stanko, mislav, ana],	[stanko, ana, mislav],	mislav
  *
  * ------------------------------
  * 입출력 예 설명
@@ -58,12 +60,22 @@ public class HASH001 {
 
         public String solution(String[] participant, String[] completion) {
             String answer = "";
-            Map<String, Integer> hm = new HashMap<String, Integer>();
-            for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
-            for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-            for (String key : hm.keySet()) {
-                if (hm.get(key) != 0){
+            Map<String, Integer> marathon = new HashMap<>();
+
+            // 참가자의 이름을 기준으로 값을 1씩 증가 ( 동일한 참자가는 중복 카운트 )
+            for(String s : participant){
+                marathon.put(s, marathon.getOrDefault(s, 0) + 1);
+            }
+
+            // 마라톤 완주자의 이름을 기준으로 값을 1씩 감소
+            for(String s : completion){
+                marathon.put(s, marathon.get(s) -1);
+            }
+
+            // 0이 아닌 경우 완주하지 못한 경우 이므로 출력
+            for(String key : marathon.keySet()){
+                if(marathon.get(key) != 0){
                     answer = key;
                 }
             }
@@ -86,23 +98,6 @@ public class HASH001 {
             return participant[participant.length-1];
         }
     }
-
-    public static class Solution2 {
-
-        public String solution(String[] participant, String[] completion) {
-
-            Arrays.sort(participant);
-            Arrays.sort(completion);
-
-            int i;
-            for(i=0; i<participant.length-1; i++){
-                if(!participant[i].equals(completion[i])) break;
-            }
-
-            return participant[i];
-        }
-    }
-
 
 
 }
