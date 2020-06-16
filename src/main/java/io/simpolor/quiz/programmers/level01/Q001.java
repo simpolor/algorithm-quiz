e5eeb2ac7a1dfbc11c61271be483ff252fe6f782
Simpolor <simpolor@gmail.com>
2020-06-16 23:50:15 +0900

    .

--- src/main/java/io/simpolor/quiz/programmers/level01/Q001.java
+++ src/main/java/io/simpolor/quiz/programmers/level01/Q001.java
@@ -1,5 +1,7 @@
 package io.simpolor.quiz.programmers.level01;
 
+import java.util.Stack;
+
 public class Q001 {
 
 
@@ -81,8 +83,14 @@ public class Q001 {
      */
     public static void main(String[] args){
 
-        int[][] board = new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] moves = new int[]{1,5,3,5,1,2,1,4};
+        int[][] board = new int[][]{
+                {0,0,0,0,0},
+                {0,0,1,0,3},
+                {0,2,5,0,1},
+                {4,2,4,4,2},
+                {3,5,1,3,1}};
+
+        int[] moves = new int[]{ 1, 5, 3, 5, 1, 2, 1, 4 };
 
         Solution solution = new Solution();
         int result = solution.solution(board, moves);
@@ -94,8 +102,108 @@ public class Q001 {
 
         public int solution(int[][] board, int[] moves) {
 
-            return 0;
+            int answer = 0;
+
+            Stack<Integer> stack = new Stack<>();
+
+            // 크레인을 움직인 수 만큼 반복
+            for(int i=0; i<moves.length; i++){
+
+                // 실제로 인덱스는 0부터 시작하므로 -1
+                int pickup = moves[i] - 1;
+
+                // 현재 인형
+                for(int j=0; j<board.length; j++){
+                    int doll = board[j][pickup];
+
+                    if(doll != 0){
+                        board[j][pickup] = 0;
+
+                        if(!stack.empty()){
+                            int bump = stack.peek();
+                            if(bump == doll){
+                                stack.pop();
+                                answer += 2;
+                                break;
+                            }
+                        }
+
+                        stack.push(doll);
+                        break;
+                    }
+                }
+            }
+
+            return answer;
         }
 
+    }
+
+    public static class Solution1 {
+
+        public int solution(int[][] board, int[] moves) {
+            int count = 0;
+            Stack<Integer> st = new Stack<Integer>();
+            for(int i = 0; i < moves.length; i++) {        //뽑는 인형 위치
+                int pickUp = moves[i]-1;                //moves의 위치를 board 배열의 인덱스로 치환
+                int temp = 0;                            //뽑은 인형
+                for(int j = 0; j < board.length; j++) {    //위에서 아래로 내려가면서 인형을 뽑으면 그부분을 0으로 만들어 주고 break
+                    if(board[j][pickUp] != 0) {
+                        temp = board[j][pickUp];        //뽑은 인형
+                        board[j][pickUp] = 0;
+                        break;
+                    }
+                }
+                if(temp != 0) {                            //1.인형을 뽑았는데
+                    if(st.isEmpty()) {                    //2-1.바구니가 비었으면
+                        st.push(temp);                    //바구니에 push
+                    }else {                                //2-2.바구니가 안비었으면
+                        if(st.peek() == temp) {            //3-1.바구니 최근에 담긴게 뽑은 인형이랑 같으면
+                            st.pop();                    //담긴 인형을 없애고
+                            count++;                    //터트림
+                        }else {                            //3-2.바구니에 담긴 인형이 뽑은인형이랑 다르면
+                            st.push(temp);                //뽑은 인형을 바구니에 추가
+                        }
+                    }
+                }
+            }
+            return count*2;                                //없어진 인형은 짝을 이루므로 *2
+        }
+
+    }
+
+    public static class Solution2 {
+        public static int solution(int[][] board, int[] moves) {
+            int answer = 0;
+            Stack<Integer> st = new Stack<>();
+            for (int i = 0; i < moves.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[j][moves[i] - 1] != 0) {
+                        //해당 위치에 인형이 있는 경우
+                        if (st.empty()) {
+                            //인형을 담을 스택이 비어있는 경우
+                            st.push(board[j][moves[i] - 1]);
+                        } else {
+                            //스택이 비어있지 않는 경우
+                            if (st.peek() == board[j][moves[i] - 1]) {
+                                //st.peek()을 사용하여 집어올린 인형과 값을 비교해 두 값이 같은 경우에는
+                                st.pop();
+                                //스택에서 인형을 제거한 뒤
+                                answer += 2;
+                                // answer를 2 증가시켜준다. (인형 2개가 사라지기 때문)
+                            } else {
+                                //st.peek()을 사용하여 집어올린 인형과 값을 비교해 두 값이 다른 경우에는
+                                st.push(board[j][moves[i] - 1]);
+                                //스택에 인형을 넣어준다.
+                            }
+                        }
+                        //일련의 작업이 끝난 뒤 인형을 집어올린 좌표에는 더 이상 인형이 없으므로 0을 저장한다.
+                        board[j][moves[i] - 1] = 0;
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
     }
 }
