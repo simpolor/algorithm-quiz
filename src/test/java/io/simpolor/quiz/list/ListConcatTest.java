e97f05e2c4aa932535bd45b8d62afebf46c09b24
Simpolor <simpolor@gmail.com>
2020-09-07 23:49:08 +0900

    .

---
+++ src/test/java/io/simpolor/quiz/list/ListConcatTest.java
@@ -0,0 +1,66 @@
+package io.simpolor.quiz.list;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.ListUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ListConcatTest {
+
+    @Test
+    public void testAddAll(){
+
+        List<String> list1 = Arrays.asList("나봉수", "라봉팔");
+        List<String> list2 = Arrays.asList("김형희", "하지민");
+        System.out.println("list1 : "+list1);
+        System.out.println("list2 : "+list2);
+
+        List<String> concatList = new ArrayList<>();
+        concatList.addAll(list1);
+        concatList.addAll(list2);
+        System.out.println("결과:  "+concatList);
+    }
+
+    @Test
+    public void testListUtilsByUnion(){
+
+        List<String> list1 = Arrays.asList("나봉수", "라봉팔");
+        List<String> list2 = Arrays.asList("김형희", "하지민");
+        System.out.println("list1 : "+list1);
+        System.out.println("list2 : "+list2);
+
+        List<String> concatList = ListUtils.union(list1, list2);
+        System.out.println("결과: "+concatList);
+    }
+
+    @Test
+    public void testGuavaByConcat(){
+
+        List<String> list1 = Arrays.asList("나봉수", "라봉팔");
+        List<String> list2 = Arrays.asList("김형희", "하지민");
+        System.out.println("list1 : "+list1);
+        System.out.println("list2 : "+list2);
+
+        Iterable<String> joinedIterable = Iterables.unmodifiableIterable(Iterables.concat(list1, list2));
+        List<String> concatList = Lists.newArrayList(joinedIterable);
+        System.out.println("결과: "+concatList);
+    }
+
+    @Test
+    public void testStreamByConcat(){
+
+        List<String> list1 = Arrays.asList("나봉수", "라봉팔");
+        List<String> list2 = Arrays.asList("김형희", "하지민");
+        System.out.println("list1 : "+list1);
+        System.out.println("list2 : "+list2);
+
+        List<String> concatList = Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+        System.out.println("결과: "+concatList);
+    }
+}
--- src/test/java/io/simpolor/quiz/map/MapPrintTest.java
+++ src/test/java/io/simpolor/quiz/map/MapPrintTest.java
@@ -94,4 +94,39 @@ public class MapPrintTest {
             System.out.printf("value : %d%n", value);
         }
     }
+
+    @Test
+    public void testForEach(){
+
+        Map<String, Integer> map = new HashMap();
+        map.put("김철수", 17);
+        map.put("안영미", 19);
+        map.put("나영희", 16);
+
+        map.forEach((key, value) -> System.out.printf("key : %s, value : %d%n", key, value));
+    }
+
+    @Test
+    public void testForEachByEntrySet(){
+
+        Map<String, Integer> map = new HashMap();
+        map.put("김철수", 17);
+        map.put("안영미", 19);
+        map.put("나영희", 16);
+
+        map.entrySet().forEach((entry)
+                -> System.out.printf("key : %s, value : %d%n", entry.getKey(), entry.getValue()));
+    }
+
+    @Test
+    public void testForEachByKeySetAndValue(){
+
+        Map<String, Integer> map = new HashMap();
+        map.put("김철수", 17);
+        map.put("안영미", 19);
+        map.put("나영희", 16);
+
+        map.keySet().forEach((key) -> System.out.printf("key : %s%n", key));
+        map.values().forEach((value) -> System.out.printf("value : %d%n", value));
+    }
 }
